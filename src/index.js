fda1a7fed1fbe5a222ee82bb017e86912de9abc6
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-12 18:21:14 +0200

    Greeting state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+//import App from './components/App/App';
 import Greeting from './components/Greeting/Greeting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.Fragment>
-      <App />
       <Greeting name="Alex" country="USA" />
       <Greeting name="John" country="USA" />
       <Greeting name="Jane" country="GB" />

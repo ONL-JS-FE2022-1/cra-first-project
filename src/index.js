0583428a3c0031d304399de8332325e1202e234f
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-13 20:54:17 +0200

    Data exchange

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ToDoList from './components/ToDoList/ToDoList';
+import ToDoList from './components/ToDoList/ToDoList'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

e64944a8eb9cff6f29e9eafffa4cea9ff036121b
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2022-12-20 19:58:16 +0200

    PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-import ToDoList from './ToDoList/ToDoList'
+import UserCard from "./UsersList/UserCard";
+
 
 
 function App() {
+    const user = {
+        name: {
+            first: 'John',
+            last: 'Doe'
+        },
+        email: 'test@test.com',
+        picture: {
+            large: 'https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg'
+        }
+    }
+
     return(
-        <ToDoList />
+        <UserCard user={user} />
     )
 }
 
--- src/components/UsersList/UserCard.js
+++ src/components/UsersList/UserCard.js
@@ -1,16 +1,40 @@
+import PropTypes from 'prop-types';
+import React from 'react';
 import './style.css';
 
-function UserCard(props) {
+class UserCard extends React.Component {
 
-    const {user: {name: {first: firstName, last: lastName}, email, picture: {large: imgSrc}}} = props;
+    render() {
+        const {user: 
+            {name: {first: firstName, last: lastName}, 
+            email, 
+            picture: {large: imgSrc}
+        }
+        } = this.props;
 
-    return(
-        <div className="card-wrapper">
-            <img src={imgSrc} alt={`${firstName} ${lastName}`} className="user-img"></img>
-            <h1>{firstName} {lastName}</h1>
-            <p>{email}</p>
-        </div>
-    )
+
+        return (
+            <div className="card-wrapper">
+                <img src={imgSrc} alt={`${firstName} ${lastName}`} className="user-img"></img>
+                <h1>{firstName} {lastName}</h1>
+                <p>{email}</p>
+            </div>
+        )
+    }
+
+}
+
+UserCard.propTypes = {
+    user: PropTypes.shape({
+        name: PropTypes.shape({
+            first: PropTypes.string,
+            last: PropTypes.string
+        }),
+        email: PropTypes.string.isRequired,
+        picture: PropTypes.shape({
+            large: PropTypes.string.isRequired
+        })
+    }).isRequired
 }
 
 export default UserCard;

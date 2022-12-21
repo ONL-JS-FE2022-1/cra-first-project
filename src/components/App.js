a1a59a889ff6c9e789c052f9a2cc71cdbcc210d2
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2022-12-21 18:38:44 +0200

    Prop children

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
-import UserCard from "./UsersList/UserCard";
+import List from "./List";
 
 
 
 function App() {
-    const user = {
-        name: {
-            first: 'John',
-            last: 'Doe'
-        },
-        email: 'test@test.com',
-        picture: {
-            large: 'https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg'
-        }
-    }
-
     return(
-        <UserCard user={user} />
+        <List />
     )
 }
 

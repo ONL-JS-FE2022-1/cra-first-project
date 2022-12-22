0ba30e3be60c4f15080efe0e0c3c89a01aefd210
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2022-12-22 20:50:00 +0200

    React Router Dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,82 @@
+import AlohaDashboard from "./AlohaDashboard/AlohaDashboard";
+import UsersList from "./UsersList/UserList";
+import CounterPage from "./Counter/CounterPage";
+import Scene from "./Scene/Scene";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <button
-        onClick={() => {
-          window.location.assign("https://www.npmjs.com/");
-        }}
-      >
-        Assign
-      </button>
-      <button
-        onClick={() => {
-          window.location.reload();
-        }}
-      >
-        Reload
-      </button>
-      <button
-        onClick={() => {
-          window.location.replace("https://developer.mozilla.org/ru/");
-        }}
-      >
-        Replace
-      </button>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="aloha">Go to aloha</Link>
+          </li>
+          <li>
+            <Link to="users">Go to users</Link>
+          </li>
+          <li>
+            <Link to="counter">Go to counter</Link>
+          </li>
+          <li>
+            <Link to="scene">Go to scene</Link>
+          </li>
+        </ul>
+      </nav>
 
-      <p>lorem ipsum dolor sit amet</p>
-      <button
-        onClick={() => {
-          window.navigator.clipboard.writeText("lorem ipsum dolor sit amet");
-        }}
-      >
-        Click to copy text
-      </button>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/aloha" element={<AlohaDashboard />} />
+        <Route path="/users" element={<UsersList />} />
+        <Route path="/counter" element={<CounterPage />} />
+        <Route path="/scene" element={<Scene />} />
 
-      <button onClick={() => {
-        const text = window.navigator.clipboard.readText();
-        console.log(text);
-      }}>Read text from buffer</button>
-    </>
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
+        tortor at fringilla egestas. Praesent vehicula maximus nisl at lobortis.
+        Duis molestie, mi a gravida.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
+        tortor at fringilla egestas. Praesent vehicula maximus nisl at lobortis.
+        Duis molestie, mi a gravida.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
+        tortor at fringilla egestas. Praesent vehicula maximus nisl at lobortis.
+        Duis molestie, mi a gravida.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
+        tortor at fringilla egestas. Praesent vehicula maximus nisl at lobortis.
+        Duis molestie, mi a gravida.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus
+        tortor at fringilla egestas. Praesent vehicula maximus nisl at lobortis.
+        Duis molestie, mi a gravida.
+      </p>
+    </>
+  );
+};
+
+const NotFound = () => {
+    return (
+        <>
+        <h1>404 PAGE NOT FOUND</h1>
+        <p>Please, try again.</p>
+        </>
+    )
+}

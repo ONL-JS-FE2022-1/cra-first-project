390069afcfee14b93b119dc163f2fc4e3bdac94f
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2022-12-23 19:34:34 +0200

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,54 @@
 import DataProvider from "./DataProvider";
-import PhoneLoader from "./PhoneLoader";
-import TVLoader from "./TVLoader";
 
 function App() {
   return (
-    <>    
-      {/* <PhoneLoader />
-      <TVLoader /> */}
+    <>
+      <DataProvider
+        loadData={() => {
+          return fetch("./phones.json").then((response) => response.json());
+        }}
+      >
+        {(state) => {
+          const { data, isLoading, isError } = state;
+          return (
+            <>
+              {isLoading && <div>Loading...</div>}
+              {isError && <div>Error happening!</div>}
+              <ul>
+                {data.map((data, index) => (
+                  <li key={index}>
+                    {data.brand} - {data.model}. Price: {data.price}
+                  </li>
+                ))}
+              </ul>
+            </>
+          );
+        }}
+      </DataProvider>
 
-
-
-
-<DataProvider loadData={()=>{
-                   return fetch('./phones.json')
-                    .then((response)=>response.json())}}>
-                      
-                    {(state)=>{
-                        const {data, isLoading, isError} = state;
-                        return (
-                            <>
-                            {isLoading && <div>Loading...</div>}
-                            {isError && <div>Error happening!</div>}
-                            <ul>
-                                {data.map((data, index)=>
-                                <li key={index}>{data.brand} - {data.model}. Price: {data.price}</li>)}
-                            </ul>
-                            </>
-                        );
-                    }}
-        </DataProvider>
-
-
-
-
-
+      <DataProvider
+        loadData={() => {
+          return fetch("./tv.json").then((response) => response.json());
+        }}
+      >
+        {(state) => {
+          const { data, isLoading, isError } = state;
+          return (
+            <>
+              {isLoading && <div>Loading...</div>}
+              {isError && <div>Error happening!</div>}
+              <ul>
+                {data.map((data, index) => (
+                  <li key={index}>
+                    АКЦІЯ!!! {data.brand} - {data.model}. Price: {data.price}
+                  </li>
+                ))}
+              </ul>
+            </>
+          );
+        }}
+      </DataProvider>
     </>
-
-
-
   );
 }
 

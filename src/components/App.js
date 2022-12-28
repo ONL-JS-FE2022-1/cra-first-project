bbda3b77775adc285418368a305754e83fc1daa0
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2022-12-28 18:55:29 +0200

    Hook: useState()

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import SignUpForm from "./LoginForm";
+import NewCounter from "./NewCounter";
+import Counter from "./NewCounter/counter";
+
 class App extends React.Component {
   render() {
     return (
-      <SignUpForm />
+      <Counter />
     );
   }
 }
